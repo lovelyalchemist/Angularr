b8abff7251cafe0f73e77266b9116dffd369f1cf
Ebruesra <ebruesra06@gmail.com>
2019-03-04 15:04:17 +0300

    Project
    Added Project

---
+++ Practice-Project/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { StarWarsService } from './star-wars.service';
+import { LogService } from './log.service';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+const routes = [
+  { path: 'characters', component: TabsComponent, children: [
+    { path: '', redirectTo: 'all', pathMatch: 'full' },
+    { path: ':side', component: ListComponent }
+  ]},
+  { path: 'new-character', component: CreateCharacterComponent},
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent,
+    CreateCharacterComponent,
+    NotFoundComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
+  providers: [StarWarsService, LogService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Practice-Project/src/app/star-wars.service.ts
@@ -0,0 +1,66 @@
+import { LogService } from './log.service';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+import { HttpClient } from '@angular/common/http';
+import { Response } from 'selenium-webdriver/http';
+
+
+
+@Injectable()
+export class StarWarsService {
+  private characters = [
+    { name: 'Luke Skywalker', side: ''},
+    { name: 'Darth Vader', side: ''},
+  ];
+
+  private logService: LogService;
+  charactersChanged = new Subject<void>();
+  HttpClient: HttpClient;
+// subject bir object reactiveden
+  constructor(logService: LogService, HttpClient: HttpClient) {
+    this.logService = logService;
+    this.HttpClient = HttpClient;
+  }
+
+  fetchCharacters() {
+    this.HttpClient.get('https://swapi.co/api/people/')
+    .map((response: Response) => {
+      return response.json();
+    })
+    .subscribe(
+      (data) => {
+        console.log(data);
+      }
+    );
+  }
+
+  getCharacters(chosenList) {
+    if (chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === chosenList;
+    });
+}
+onSideChosen(charInfo) {
+  const pos = this.characters.findIndex((char) => {
+    return char.name === charInfo.name;
+  });
+  this.characters[pos].side = charInfo.side;
+  this.charactersChanged.next(); // değişen karakter özellikleri
+  this.logService.writeLog('Changed side of ' + charInfo.name + ', new side ' + charInfo.side);
+}
+// -1 demek aynısından var demek
+  addCharacter(name, side) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === name;
+    });
+    if (pos !== -1) {
+      return;
+    }
+    const newChar = {name: name, side: side};
+    this.characters.push(newChar);
+  }
+}
