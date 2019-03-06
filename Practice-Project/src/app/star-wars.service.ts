c98d9de6f5676aa232d583fbb49763d0141f5ed4
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2019-03-06 13:05:03 +0300

    Bu hatayı yapmış olamazsın
    backendden de baktım result değil results olacak, bende sorunsuz çalışıyor şuan

--- Practice-Project/src/app/star-wars.service.ts
+++ Practice-Project/src/app/star-wars.service.ts
@@ -30,7 +30,7 @@ export class StarWarsService {
     this.http.get('https://swapi.co/api/people/')
     .map((response: Response ) => {
       const data = response;
-      const extractedChars = data.result;
+      const extractedChars = data.results;
       const chars = extractedChars.map((char) => {
              return {name: char.name, side: ''};
           });
